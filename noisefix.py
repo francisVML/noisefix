2cc6a99f74cf62c9d89762cd02f43a3de1f61ff3
francisVML <44818679+francisVML@users.noreply.github.com>
2020-03-26 11:32:06 -0500

    Add files via upload

---
+++ noisefix.py
@@ -0,0 +1,52 @@
+# Modify JSON Cal file
+from ctypes import *
+import json
+import requests
+import os.path
+
+url = 'http://localhost/api/sensors/0/'
+#replace path with current Documents folder path in the directory
+path = 'C:/Users/fnatividad/Documents/'
+
+level = [ 1,0,0,0,0,0,0,0,0,0,0 ] 
+
+#  Serial number
+serial = requests.get(url+'name').json()
+print (serial)
+
+data = requests.get(url+'calibration')
+calfile = data.json()
+with open(path+serial+"og.json", "w") as data_file:
+    json.dump(calfile, data_file)
+print ("Original Calfile Downloaded")
+
+# while loop to multiply existing calfile with the multipliers
+c = 0
+hyst = calfile ['filters'] [1] ['samples'] [c]    
+j = 0
+for cell in hyst:
+    multiplier = mult [j] + ((1 - mult [j])*level [c])
+    calfile ['filters'] [1] ['samples'] [c] [j] = cell * multiplier
+    j += 1
+# double conductances at the first frame
+
+zero = calfile ['filters'] [1] ['samples'] [c]
+#print (hyst)
+mod = [None] * len (zero)
+j = 0
+for cell in zero:
+    mod [j] = 70
+    j += 1
+calfile ['filters'] [1] ['samples'] [c] = mod
+
+
+# Load modified cal file into sensor
+requests.put(url+"calibration", json=calfile) 
+
+# save a copy of the modified calibration file
+with open(path+serial+"ns.json", "w") as data_file:
+    json.dump(calfile, data_file)
+    
+print('Done')
+
+input ("Press Enter to Exit") 
